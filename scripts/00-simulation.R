7ccc51a9199e31717e63bcca22157bb1de786f1b
zhongyu <zhongyu.huang@mail.utoronto.ca>
2022-04-03 03:12:47 -0400

    add simulation of the plan

---
+++ scripts/00-simulation.R
@@ -0,0 +1,18 @@
+# We will convert the information from the “Median age at first birth” table on
+# page 44 of the Philippines DHS final report in 1998.
+
+# what is needed is a table with a column for current age, residence, region, 
+# education, and median age of first birth. 
+
+--------------------------------------------------------
+current_age | residence | region | education | median_age
+--------------------------------------------------------
+25-29       | urban     | Ilocos | "no education" | 24.5
+--------------------------------------------------------
+   ""       |  ""       | ""     |   ""          |  ""  
+--------------------------------------------------------
+   ""      |   ""      |   ""    |  ""           |   ""     
+--------------------------------------------------------
+  .             
+  .
+  .
