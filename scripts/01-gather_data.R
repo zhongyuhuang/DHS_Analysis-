c443617ebbd029a85ea995868f73fb6bab6bcee3
zhongyu <zhongyu.huang@mail.utoronto.ca>
2022-04-08 05:06:38 -0400

    changed the dataset

--- scripts/01-gather_data.R
+++ scripts/01-gather_data.R
@@ -21,55 +21,44 @@ pdf_dhs <- tibble(raw_data = pdf_dhs)
 
 
 # grab the page that is of interest
-dhs1 <- pdf_dhs %>% slice(69)
+dhs1 <- pdf_dhs %>% slice(70)
 
 
 # separate the row and get rid of title and footnotes of the table
 dhs2 <- dhs1 |> separate_rows(raw_data, sep = "\\n", convert = FALSE)
-dhs3 <- dhs2 %>% slice(11:37)
+dhs3 <- dhs2 %>% slice(11:47)
 
 
 #separate this into separate columns
 dhs4 <- dhs3 %>% mutate(data = str_squish(raw_data)) %>% separate(col = raw_data, 
                   into = c("BG_inf", 
-                           "25-29", 
-                           "30-34", 
-                           "35-39", 
-                           "40-44", 
-                           "45-49", 
-                           "25-49"), 
-                  sep = "[[:space:]]{2,}", 
+                           "PT_Mom", 
+                           "PT_Pgt", 
+                           "PT_bear", 
+                           "NumOfTeen"), 
+                  sep = "[[:space:]]{3,}", 
                   remove = FALSE
   )
 
 
-# rewrite numbers that has ','
-dhs4[3,3] = "22.3"
-dhs4[3,4] = "22.3"
-dhs4[3,5] = "22.3"
-dhs4[3,8] = "22.2"
-dhs4[6,5] = "24.7"
-dhs4[9,6] = "22.2"
-dhs4[15,3] = "22.9"
-dhs4[15,8] = "21.6"
-dhs4[9,6] = "22.2"
-dhs4[18,8] = "22.7"
-dhs4[9,6] = "22.2"
-dhs4[21,6] = "20.9"
-dhs4[24,3] = "19.9"
-dhs4[24,5] = "20.4"
-dhs4[24,6] = "20.4"
-dhs4[27,6] = "26.3"
+# rewrite incorrect numbers and numbers that has ','
+dhs4[10,6] = "1701"
+dhs4[11,6] = "1223"
+dhs4[17,5] = "10.6"
+dhs4[32,3] = "17.3"
+dhs4[34,6] = "1962"
+dhs4[37,6] = "2924"
+
 
 
 # make the numbers into actual numbers
 options(digits=3)
-dhs4$`25-29` <-as.numeric(dhs4$`25-29`)
-dhs4$`30-34` <-as.numeric(dhs4$`30-34`)
-dhs4$`35-39` <-as.numeric(dhs4$`35-39`)
-dhs4$`40-44` <-as.numeric(dhs4$`40-44`)
-dhs4$`45-49` <-as.numeric(dhs4$`45-49`)
-dhs4$`25-49` <-as.numeric(dhs4$`25-49`)
+dhs4$`25-29` <-as.numeric(dhs4$BG_inf)
+dhs4$`30-34` <-as.numeric(dhs4$PT_Mom)
+dhs4$`35-39` <-as.numeric(dhs4$PT_Pgt)
+dhs4$`40-44` <-as.numeric(dhs4$PT_bear)
+dhs4$`45-49` <-as.numeric(dhs4$NumOfTeen)
+
 
 # save the output in outputs/data/raw_data.csv
 
