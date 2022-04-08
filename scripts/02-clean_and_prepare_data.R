1e8745c56475b89d6498d4f8e8d79b6da1b84f83
zhongyu <zhongyu.huang@mail.utoronto.ca>
2022-04-08 05:22:55 -0400

    cleaned and prepare the data

--- scripts/02-clean_and_prepare_data.R
+++ scripts/02-clean_and_prepare_data.R
@@ -18,24 +18,26 @@ dhs_data <- readr::read_csv("outputs/data/raw_data.csv")
 # Just keep some variables that may be of interest  
 reduced_data <- 
   dhs_data %>% 
-  select(-raw_data,-data)
+  select(BG_inf,PT_Mom,PT_Pgt,PT_bear,NumOfTeen)
 
 # clean up empty rows
-reduced_data <- reduced_data[-c(4, 22), ]
+reduced_data <- reduced_data[-c(1,8,12,30,36), ]
 
 #test and check the data
-#check that "25-49" stores the true average
+#check the sum of number of teens in each section matches the Total
 follow_up <- 
   reduced_data |> 
-  mutate(check_sum = (`25-29`+`30-34`+`35-39`+`40-44`+`45-49`)/5,
-         totals_match = if_else(`25-49` == check_sum, 1, 0)
+  mutate(check_sum = (2924),
+         totals_match = if_else(reduced_data[2,5]+reduced_data[3,5]+ 
+                                  reduced_data[4,5]+ reduced_data[5,5]
+                                +reduced_data[6,5]== check_sum, 1, 0)
   ) |> 
   filter(totals_match == 0)
 
 # check by using pointblank
 agent <-
   create_agent(tbl = reduced_data) |>
-  col_is_numeric(columns = 3:8) |> 
+  col_is_numeric(columns = 2:5) |> 
   interrogate()
 
 #save the output to 'outputs/data/cleaned_data.csv’
