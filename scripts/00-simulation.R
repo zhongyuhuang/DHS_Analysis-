c84e9df67aa0fcb4ebcd553b8855b7ac42be1ef9
zhongyu <zhongyu.huang@mail.utoronto.ca>
2022-04-10 12:32:46 -0400

    simulated data

--- scripts/00-simulation.R
+++ scripts/00-simulation.R
@@ -1,22 +1,21 @@
-# We will convert the information from the “Median age at first birth” table on
+
+# We will simulate data that we might find in the “Median age at first birth” table on
 # page 44 of the Philippines DHS final report in 1998.
 
-# what is needed is a table with rows indicate, residence, region, 
-# education, and median age of first birth. 
+#set seed  
+set.seed(100270)
 
----------------------------------------------------------
-Residence | 25-29  | 30-34 | 35-39 | 40-44 | 45-49| 25-49
---------------------------------------------------------
-urban     | ""     | ""    | ""    | ""    |   "" | ""  
---------------------------------------------------------
-""        |  ""    | ""    |   ""  |  ""   | ""   |""
---------------------------------------------------------
-Region    |   ""   |   ""  |  ""   |   ""  |  ""  | ""
---------------------------------------------------------
-  ""      |  ""    | ""    |   ""  |  ""   |""    |
---------------------------------------------------------
-Education |  ""    | ""    |   ""  |  ""   | ""   |
---------------------------------------------------------
-  .             
-  .
-  .
+number_of_observations <- 5
+
+#simulate the data
+simulated_data <- 
+  data.frame(
+    background=rep(letters[1:5], each = 1 ),
+    percentage_who_are_mothers=runif(n=5, min=0, max=1),
+    percentage_who_begun_childbearing=runif(n=5, min=0, max=1),
+    percentage_who_are_pregnant=runif(n=5, min=0, max=1)
+    
+  )
+
+# print the simulated data
+simulated_data
